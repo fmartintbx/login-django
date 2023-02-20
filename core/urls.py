f586ddcbf816b6e7007260aef817c7fa1a86fff2
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2023-02-20 01:10:22 -0300

    login

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from.views import home, products,exit
+
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('products/',products,name='products'),
+    path('logout/',exit,name='exit'),
+
+
+    
+]
